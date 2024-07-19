7c5b8bba3cb44dff05c1254b60a16fce33b7dd10
salma25128 <salmabasuony3@gmail.com>
2024-07-19 23:52:18 +0300

    owl_dasboard

--- awesome_dashboard/__manifest__.py
+++ awesome_dashboard/__manifest__.py
@@ -1,18 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Awesome Dashboard",
-
-    'summary': """
-        Starting module for "Discover the JS framework, chapter 2: Build a dashboard"
-    """,
-
-    'description': """
-        Starting module for "Discover the JS framework, chapter 2: Build a dashboard"
-    """,
-
-    'author': "Odoo",
     'website': "https://www.odoo.com/",
-    'category': 'Tutorials/AwesomeDashboard',
     'version': '0.1',
     'application': True,
     'installable': True,
